eaa0a7853014cb3be356e250936e89cfe75525fa
AnasSardarzai <anassardarzai361@outlook.de>
2025-09-24 14:10:33 +0200

    verbessere HTML der Karten-Elemente

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -9,23 +9,29 @@ animals_data = load_data('animals_data.json')
 
 
 output = ''
-for i in animals_data:
+for animal in animals_data:
     output += '<li class="cards__item">\n'
-    output += f"Name: {i['name']}<br/>\n"
-    output += f"Diet: {i['characteristics']['diet']}<br/>\n"
-    output += f"Location: {i['locations'][0]}<br/>\n"
+    output += f'  <div class="card__title">{animal["name"]}</div>\n'
+    output += '  <p class="card__text">\n'
+    output += f'      <strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
+    output += f'      <strong>Location:</strong> {animal["locations"][0]}<br/>\n'
 
-    if "type" in i["characteristics"]:
-        output += f"Type: {i['characteristics']['type']}<br/>\n"
 
+    if "type" in animal["characteristics"]:
+        output += f'      <strong>Type:</strong> {animal["characteristics"]["type"]}<br/>\n'
+
+    output += '  </p>\n'
     output += '</li>\n'
+
 
 with open('animals_template.html', 'r') as file:
     html_template = file.read()
 
+
 html_content = html_template.replace("__REPLACE_ANIMALS_INFO__", output)
 
-with open('animals_template.html', 'w') as file:
+
+with open('animals.html', 'w') as file:
     file.write(html_content)
 
 print("HTML-Datei 'animals.html' wurde erstellt!")
